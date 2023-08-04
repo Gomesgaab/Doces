a6a229c8cc441ad3d6d6ac0481ef012c3695bb63
Gomesgaab <gabrielgabiga870@gmail.com>
2023-08-03 21:27:15 -0300

    first commit

---
+++ MaquinaDocos/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaquinaDocos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Conecta com a classe Control Produto
+            ControlPagamento controlPgto = new ControlPagamento();
+
+            //Chamar o método principal daquela classe
+            controlPgto.Operacao();
+
+            Console.ReadLine();//Manter a janela aberta
+        }//fim do método main
+    }// fim classe
+    
+}// fim program
